0ab0b75c1c4662ffebc8fc9626dbe90662ba2f3f
Fadiah Mumtaz Andevi <71587146+fadimtz@users.noreply.github.com>
2022-05-19 11:30:48 +0700

    Update main.c

--- Tugas Besar/main.c
+++ Tugas Besar/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-#define MAX_SIZE 4096
+#define MAX_SIZE 4096 
 
 int bitsSize;    //banyaknya variabel
 int limit;       //angka terbesar pada minterm
